ecf120cd6715bdfb8cac7d2fa214c9f663f25507
Ana Cris 1125 <amejialalinde@hotmail.com>
2025-08-22 13:31:40 +0800

    Nueva actualizacion de mi proyecto

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -4,7 +4,7 @@ const auth = require('../middlewares/auth');
 
 router.get('/', auth, getCards);
 router.post('/', auth, createCard);
-router.delete('/:cardId', deleteCard);
+router.delete('/:cardId', auth, deleteCard);
 router.put('/:cardId/likes', auth, likeCard);
 router.delete('/:cardId/likes', /*auth,*/ dislikeCard);
 
